c69e8985a7e729574f0176cf721dbb0bd9e9d3d1
Marketing-Newtec <marketing.distribuidoragamma@gmail.com>
2026-03-13 11:00:36 -0300

    Form ocultado V3

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { Hero } from './components/Hero';
 import { Features } from './components/Features';
 import { ProductSection } from './components/ProductSection';
 import { OriginSection } from './components/OriginSection';
-import { HRSection } from './components/HRSection';
+// import { HRSection } from './components/HRSection';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 
