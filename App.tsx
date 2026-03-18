10dc5fa733769761260ec225c5f82c6b37a0c382
Marketing-Newtec <marketing.distribuidoragamma@gmail.com>
2026-03-18 13:10:02 -0300

    menu-about

--- App.tsx
+++ App.tsx
@@ -31,7 +31,7 @@ const App: React.FC = () => {
         <Features />
         <ProductSection />
         <OriginSection />
-        <HRSection />
+        <AboutSection />
       </main>
 
       <Footer />
